3dc32d3c44880cac0f31200d23675959abb4232f
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-07-05 13:52:57 +0200

    Wrote computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('active');
+const rock = 'rock'
+    , paper = 'paper'
+    , scissors = 'scissors';
+let computerSelection = ''
+  , playerSelection = '';
+
+function computerPlay() {
+  computerSelection = Math.floor(Math.random() * 10);
+  switch(true) {
+    case (computerSelection < 4):
+      return computerSelection = rock;
+      break;
+    case (computerSelection < 7):
+      return computerSelection = paper;
+      break;
+    default:
+      return computerSelection = scissors;
+  }
+}
